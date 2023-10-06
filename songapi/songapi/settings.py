11bbd66d57b2ecbc0700cd2e7330e6bb9986ee88
sivaprakasham <sivaprakasham925@gmail.com>
2023-10-06 13:10:00 +0530

    heroku deploy commit

--- songapi/songapi/settings.py
+++ songapi/songapi/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-mew@sd(v!y775=-k(zxdsv#l%6hx4#z%sa(qdt=_u*x8i!86_b'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['vercel.com', '*']
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
